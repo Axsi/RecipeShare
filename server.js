021b2fafe52edc2b96b6fa48d6858b6e1668207c
Axsi <alex.sinn@gmail.com>
2019-09-19 12:47:10 -0700

    26th push

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const creations = require('./routes/createdpage');
 
 app.use(session({
     store: new (require('connect-pg-simple')(session))({conString: process.env.DB_URL}),
-    secret: process.env.SESSION_SECRET,
+    secret: 'omega',
     resave: false,
     saveUninitialized: false,
     cookie: {maxAge: 30 * 24 * 60 * 60 * 1000},
