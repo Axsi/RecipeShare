551fb526679a18819c472dac9f1b2c957c4e5320
Axsi <alex.sinn@gmail.com>
2019-09-19 20:42:52 -0700

    removed serviceWorker from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import CreateRecipePage from './create_recipe_page';
 import RecipePage from './recipepage';
 import CreatedPage from './createdpage'
 import EditPage from './editpage';
-// import * as serviceWorker from './serviceWorker';
 import PrivateRoute from './privateroute';
 
 const routing = (
@@ -34,4 +33,3 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
